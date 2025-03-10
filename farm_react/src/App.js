a9456b6db950a2a4e82565cdb331673cd95b5ce3
GoNXPromoters <gonxpromoters@gmail.com>
2025-03-10 21:43:14 +0530

    react proj initialized.

---
+++ farm_react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>welcome</h1>
+      <h1>Yoga</h1>
+      <h1>Dhanam</h1>
+    </>
+  );
+}
+
+export default App;
